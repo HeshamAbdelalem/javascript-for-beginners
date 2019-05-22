555fd8d200252b1cc854cc870bd53dc0bb832f4f
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2019-05-23 01:45:22 +0200

    fix: typo

--- 5-objects/7 - value vs reference/script.js
+++ 5-objects/7 - value vs reference/script.js
@@ -1,10 +1,10 @@
-//* Primitive are copied by their value
-//* Objects are copied by their reference
+//NOTE: Primitive are copied by their value
+//NOTE: Objects are copied by their reference
 
-//* Value Types:: Number, String, Boolean, Symbol, undefined, null
-//* Reference Types:: object, function, array
+//NOTE: Value Types:: Number, String, Boolean, Symbol, undefined, null
+//NOTE: Reference Types:: object, function, array
 
-//TODO: Example Num-1 :
+//! Example Num-1 :
 
 /* 
 let x = 20;
@@ -24,7 +24,7 @@ console.log(a.value); //? a will be 100
 console.log(b.value); //? b will also be 100 because it's got copied be reference
  */
 
-//TODO: Example Num-2 :
+//! Example Num-2 :
 
 let num = 10;
 
@@ -34,14 +34,14 @@ function increase(number) {
 }
 
 increase(num); //? this will be local on the function only
-console.log("Out the function", num); //? this will be still 10
+console.log('Out the function', num); //? this will be still 10
 
 let obj = { value: 20 };
 
 function increaseObj(objc) {
   objc.value++;
-  console.log("The Object inside the function:", obj.value);
+  console.log('The Object inside the function:', obj.value);
 }
 
 increaseObj(obj);
-console.log("The Object Out the function:", obj.value);
+console.log('The Object Out the function:', obj.value);
