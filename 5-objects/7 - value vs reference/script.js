e1448f90cbc20b552854ed448e84cc65cc0a1ead
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2019-05-22 19:47:40 +0200

    feat: Adds objects/ 7-value vs reference

---
+++ 5-objects/7 - value vs reference/script.js
@@ -0,0 +1,47 @@
+//* Primitive are copied by their value
+//* Objects are copied by their reference
+
+//TODO: Value Types:: Number, String, Boolean, Symbol, undefined, null
+//TODO: Reference Types:: object, function, array
+
+//TODO: Example Num-1 :
+
+/* 
+let x = 20;
+let y = x;
+
+x++;
+console.log("x value:", x); //? x will be 21
+console.log("y value:", y);//? y will still 20 
+ */
+/* 
+let a = { value: 50 };
+let b = a;
+
+a.value = 100;
+
+console.log(a.value); //? a will be 100
+console.log(b.value); //? b will also be 100 because it's got copied be reference
+ */
+
+//TODO: Example Num-2 :
+
+let num = 10;
+
+function increase(number) {
+  number++;
+  console.log("I'm inside the function:", number); //? this will get increase by 1
+}
+
+increase(num); //? this will be local on the function only
+console.log("Out the function", num); //? this will be still 10
+
+let obj = { value: 20 };
+
+function increaseObj(objc) {
+  objc.value++;
+  console.log("The Object inside the function:", obj.value);
+}
+
+increaseObj(obj);
+console.log("The Object Out the function:", obj.value);
